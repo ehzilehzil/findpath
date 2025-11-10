b73ea3e0e1d561928066fedad78fea27b50974e5
ehzilehzil <ehzilehzil@outlook.com>
2025-11-11 08:15:46 +0900

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  // base: '/findpath/', // GitHub Pages에서 사용하는 서브 디렉토리 경로
+  base: '/findpath-test/', // GitHub Pages에서 사용하는 서브 디렉토리 경로
   vite: {
     plugins: [tailwindcss()]
   },
