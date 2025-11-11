340471088fcbe1a9f32dd3821126bfd5aed4f8ce
ehzilehzil <ehzilehzil@outlook.com>
2025-11-11 13:07:24 +0900

    f: set alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import alpinejs from '@astrojs/alpinejs';
+
+
+const isBuild = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/findpath-test/', // GitHub Pages에서 사용하는 서브 디렉토리 경로
+  base: isBuild ? "/findpath/" : "", // GitHub Pages에서 사용하는 서브 디렉토리 경로
   vite: {
     plugins: [tailwindcss()]
   },
