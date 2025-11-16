4d1a0462c76b99b2934fce27d25a59c56b6ad9c6
ehzilehzil <ehzilehzil@outlook.com>
2025-11-17 08:02:53 +0900

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ const isBuild = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
-  base: isBuild ? "/findpath/" : "", // GitHub Pages에서 사용하는 서브 디렉토리 경로
+  base: isBuild ? "/findpath/" : "/", // GitHub Pages에서 사용하는 서브 디렉토리 경로
   vite: {
     plugins: [tailwindcss()]
   },
